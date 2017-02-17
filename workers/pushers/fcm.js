190bf26c9f48f1e1f8955d7e3cf0facde4852bf5
perqin <perqinxie@gmail.com>
2017-02-18 00:10:13 +0800

    Add FCM push service

--- workers/pushers/fcm.js
+++ workers/pushers/fcm.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function (clientToken, scoreObj) {
+var firebaseAdmin = require('firebase-admin');
+
+var serviceAccount = require('./../../config/service-account-credentials.json');
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: config.firebaseDatabaseUrl
+});
+
+module.exports = function (clientToken, scoresObj) {
     // TODO
+    var payload = {
+        data: {
+            sync: JSON.stringify(scoresObj)
+        }
+    };
+    return firebaseAdmin.messaging().sendToDevice(clientToken, payload).catch(function (err) {
+        console.error('Error sending message: ', err);
+    });
 };
